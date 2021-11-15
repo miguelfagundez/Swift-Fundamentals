30a89290cc310b2f44a13205360521c7fa4ea8b7
Miguel Fagundez <miguel.fagundez@capco.com>
2021-11-15 14:34:42 -0500

    Variables and Data types

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// Let & Var
+
+// Let is a constants (it cannot change)
+// Var is a variable (it can change)
+
+let myName = "Miguel"
+var age = 40
+
+age += 1
+
+// Data types
+
+// String
+let welcome: String = "Hello World!"
+
+// Integer
+let myAge: Int = 40
+
+// Floats & Doubles
+let temp: Float = 21.8    // 6 decimals
+let other: Double = 22.4  // 15 decimals
+
+// Boolean
+let isHot: Bool = true
+let isFine:Bool = false
+
+
+
+
